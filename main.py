06cf7e629e426c38b0cb5d3624e80a88f26b2f70
Juan Lozada <63521809+myjuda@users.noreply.github.com>
2024-09-19 01:57:19 -0300

    Update main.py
    Modificate cod.

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from PIL import Image
 import io
 
 input_path = "/Users/juanlozadacalderon/downloads/Remove background/macacos.jpg"
-output_path = "output.png"  # Cambiado a PNG para preservar la transparencia
+output_path = "output.png"  # Changed to PNG to preserve transparency
 
-# Abrir la imagen de entrada
+# Open imagen input
 with open(input_path, "rb") as input_file:
     input_data = input_file.read()
 
-# Remover el fondo
+# Remove background
 result_data = remove(input_data)
 
-# Convertir el resultado a una imagen de PIL
+# Convert the result to a PIL image
 output = Image.open(io.BytesIO(result_data))
 
-# Guardar la imagen de salida en formato PNG para preservar la transparencia
+# Save the output image in PNG format to preserve transparency
 output.save(output_path)
 
